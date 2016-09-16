9c91f66d7808afd58557b430077a24619f572512
Daniel Maděra <madera.dan@gmail.com>
2016-09-16 15:25:43 +0200

    Add video handing function
    Start, pause and loop features.

--- js/layout.js
+++ js/layout.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
+    // replace img tags with svg file
     $("img[src*='.svg']").each(function() {
         var img = $(this);
         $.get(img.attr("src"), function(svg) {
             img.replaceWith(svg);
         }, 'text');
     });
+
+    // video handler
+    var video = $('#bgvideo');
+    video.attr('loop', 'true');
+    video[0].play();
+    // after video is ended, show poster image 
+    /* video.bind('ended', function() {
+        video.load();
+    });*/
 });
