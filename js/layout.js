c4f2cd449d42543f939317caa5610ff90d5364d4
Daniel Maděra <madera.dan@gmail.com>
2016-09-18 22:23:06 +0200

    Add Join Us link with slider hover

--- js/layout.js
+++ js/layout.js
@@ -1,18 +1,38 @@
-$(document).ready(function() {
-    // replace img tags with svg file
+// replace svg tags with content of svg file
+var svgHandler = function() {
     $("svg[src*='.svg']").each(function() {
         var img = $(this);
         $.get(img.attr("src"), function(svg) {
             img.replaceWith(svg);
         }, 'text');
     });
+}
 
-    // video handler
+var videoHandler = function() {
     var video = $('#bgvideo');
-    video.attr('loop', 'true');
-    video[0].play();
-    // after video is ended, show poster image 
-    /* video.bind('ended', function() {
-        video.load();
-    });*/
+    setTimeout(function() {
+        // video.attr('loop', 'true');
+        // video[0].play();
+    }, 3000);
+
+    video.on('abort ended error paused', function() {
+        $(this).load();
+    });
+}
+
+$(function() {
+    $('#joinUsPanel div.hover').hide();
+    $('#joinUsPanel a').on({
+        mouseenter: function() {
+            console.log("TExt");
+            $('#joinUsPanel div.hover').show("slide", { direction: "right" }, 100);
+        },
+        mouseleave: function() {
+            $('#joinUsPanel div.hover').hide('slide', { direction: 'right' }, 100);
+        }
+    });
 });
+
+
+$(svgHandler);
+$(videoHandler);
