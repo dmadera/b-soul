17cb1626859eb43f919fddd6cae7fde29f2e4cdf
Daniel Maděra <madera.dan@gmail.com>
2018-07-14 20:25:26 +0200

    Fix responsible mentors

--- dist/js/mentors.js
+++ dist/js/mentors.js
@@ -1,17 +1,11 @@
 var toggleMentorInfo = function(div) {
-  var row1 = div.find('.row:first-child');
-  var row2 = div.find('.row:last-child');
+  var divs = div.find('[data_toggle="showed"], [data_toggle="hidden"]');
 
-  if(row1.hasClass('d-none')) {
-    row1.removeClass('d-none');
-    row2.addClass('d-none');
-  } else {
-    row1.addClass('d-none');
-    row2.removeClass('d-none');
-  }
+  divs.toggleClass('hide');
 }
 
 $(function() {
+    $('.mentors .mentor [data_toggle="hidden"]').addClass('hide');
     $('.mentors .mentor').click(function() {
         toggleMentorInfo($(this));
     });
