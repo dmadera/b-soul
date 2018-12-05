f9375d477e8fa26f6a3bb3639eed4859bd0a5447
Daniel Maděra <madera.dan@gmail.com>
2018-12-05 13:54:44 +0100

    Multiple changes

--- dist/js/global.js
+++ dist/js/global.js
@@ -34,21 +34,9 @@ $(function() {
       this.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
     });
   });
-  //$('#mediaPanel a.close').click(function() {
-  //$('#mediaPanel .content').empty();
-  //$('#mediaPanel').hide();
-  //event.stopPropagation();
-  //});
 
-  //$('#mediaPanel').click(function(event) {
-  //event.stopPropagation();
-  //});
-
-  //$(window).click(function() {
-  //if ($('#mediaPanel').is(':visible')) {
-  //$('#mediaPanel .content').empty();
-  //$('#mediaPanel').hide();
-  //event.stopPropagation();
-  //}
-  //});
+  var navMain = $('#mainNav .navbar-collapse');
+  navMain.on("click", "a:not([data-toggle])", null, function () {
+    navMain.collapse('hide');
+  });
 });
